cfa3518beb4b519f228e3f88814c788a9b8c4e48
m-ghozi <ghozi286@gmail.com>
2024-12-18 23:54:38 +0700

    ⌨ update : blink keymapping

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,18 +1,17 @@
 return {
   {
-    "akinsho/bufferline.nvim",
-    optional = true,
-    opts = {
-      options = {
-        separator_style = "thick",
-      },
-    },
-  },
-  {
     "folke/noice.nvim",
     opts = {
       lsp = { hover = { silent = true } },
       presets = { lsp_doc_border = true },
     },
+  },
+  {
+    "b0o/incline.nvim",
+    config = function()
+      require("incline").setup()
+    end,
+    -- Optional: Lazy load Incline
+    event = "VeryLazy",
   },
 }
