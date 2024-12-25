ce86084ef61fe7f798d78659f6372b57bf3ccc41
m-ghozi <ghozi286@gmail.com>
2024-12-25 15:53:16 +0700

    🛠️ fix : blink.cmp keymapping

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,9 +1,20 @@
 return {
   {
     "saghen/blink.cmp",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
-      keymap = { preset = "enter" },
+      keymap = {
+        preset = "default",
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Esc>"] = { "hide", "fallback" },
+        ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
+        ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
+      },
       completion = {
+        list = { selection = "manual" },
         trigger = {
           show_on_insert_on_trigger_character = false,
         },
