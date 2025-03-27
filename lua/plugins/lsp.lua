f2cf939d3866e55f46d43d00328f364bd972c14b
m-ghozi <ghozi286@gmail.com>
2025-03-27 23:46:44 +0700

    📦 add : php support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,5 +2,20 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    -- @type lspconfig.options
+    servers = {
+      intelephense = {
+        filetypes = { "php", "blade", "php_only" },
+        settings = {
+          intelephense = {
+            filetypes = { "php", "blade", "php_only" },
+            files = {
+              associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+              maxSize = 5000000,
+            },
+          },
+        },
+      },
+    },
   },
 }
